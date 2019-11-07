e85492e6391aadf6376cfc477681627b8c7f9234
Shivam Kumar Jha <shivam.cs.iit.kgp@gmail.com>
2019-11-08 01:25:23 +0530

    fixes #1

--- feedback.js
+++ feedback.js
@@ -6,7 +6,7 @@ try{
     }
 
     x = myframe.document.querySelectorAll('input[type="radio"]');
-    for (i = 2; i < x.length; i++){
+    for (i = 3; i < x.length; i++){
         x[i].click();
     }
 
