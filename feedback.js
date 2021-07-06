148997efe9fd3683549b109bb53fd056e34ae70c
ThePhilosopher37 <75205458+ThePhilosopher37@users.noreply.github.com>
2021-07-06 14:19:33 +0530

    added a positive, negative feedback loop

--- feedback.js
+++ feedback.js
@@ -1,18 +1,23 @@
 try{
     var x = myframe.document.querySelectorAll('textarea');
-    var i = 0;
-    for (i = 0; i < x.length; i++){
-      x[i].value="NIL";
-    }
+    var scripts = ["The teacher is great and understanding", "They have a great understanding of the subject", 
+                  "They clear doubts and make sure the students understand the concepts"];
+    x[0].value=scripts[Math.floor(Math.random() * scripts.length)];
+
+    x[1].value = "Nothing as such";
+
+    // for (i = 0; i < x.length; i++) {
+    //   x[i].value=scripts[Math.floor(Math.random() * scripts.length)];
+    // }
 
     x = myframe.document.querySelectorAll('input[type="radio"]');
     for (i = 1; i < x.length; i++){
-        if (x[i].getAttribute('name') == 'check'){
-            continue;
-        }
-        x[i].click();
+      if (x[i].getAttribute('name') == 'check') {
+        continue;
+      }
+      x[i].click();
     }
 
   }catch(err){
     console.log(err);
-  }
+}
