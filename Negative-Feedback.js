0bfbac6649d88407d6d5c1c5e8f346fa02e74e5b
Arpit Bhardwaj <proffapt@pm.me>
2022-11-13 15:45:14 +0530

    add: logic for clicking radio buttons

--- Negative-Feedback.js
+++ Negative-Feedback.js
@@ -1,1 +1,27 @@
+try{
+    radioButton = myframe.document.querySelectorAll('input[type="radio"]');
 
+    for (i = 1; i < radioButton.length-10; i+=5){
+		// Clicks `Poor` OR `Fair` everywhere
+		if (Math.floor(Math.random()*2)){
+			radioButton[i].click();
+		}else{
+			radioButton[i+1].click();
+		}
+    }
+	// Randomising selection between `Too Slow` & `Too Fast`
+	if (Math.floor(Math.random()*2)){
+		radioButton[16].click();
+	}else{
+		radioButton[20].click();
+	}
+	// Randomising selection between `Heavy` & `Very Heavy` for Efforts and Workload
+	for (i=54; i<radioButton.length; i+=5)
+	if (Math.floor(Math.random()*2)){
+		radioButton[i].click();
+	}else{
+		radioButton[i+1].click();
+	}
+  }catch(err){
+    console.log(err);
+}
