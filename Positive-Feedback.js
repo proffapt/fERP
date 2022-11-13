e8a9cf1f90c3e4d66918e4b0071b644372593c8f
Arpit Bhardwaj <proffapt@pm.me>
2022-11-13 15:00:02 +0530

    elaborating variables

--- Positive-Feedback.js
+++ Positive-Feedback.js
@@ -1,37 +1,37 @@
 try{
-    var x = myframe.document.querySelectorAll('textarea');
-    var scripts = [ "The teacher is understanding, approachable and caring", "They have deep understanding and clarity of the subject", 
+    var textBox = myframe.document.querySelectorAll('textarea');
+    var teacherStrengths = [ "The teacher is understanding, approachable and caring", "They have deep understanding and clarity of the subject", 
                     "They clear doubts and make sure the students understand the concepts", "They ensure that the speed of the lecture is perfect for everyone", 
                     "They release the slides and materials as soon as possible", "They make sure that the assignment load is not a lot on students"
                   ];
 
-    x[0].value=scripts[Math.floor(Math.random() * scripts.length)];
+    textBox[0].value=teacherStrengths[Math.floor(Math.random() * teacherStrengths.length)];
 
-    x[1].value = "Nothing as such";  // this is the default Weakness
-    x[3].value = "Nothing as such";  // replace with relevant text
+    textBox[1].value = "Nothing as such";  // this is the default Weakness
+    textBox[3].value = "Nothing as such";  // replace with relevant text
 
-    scripts = [ "The course is well structured and gave me a great understanding of the fundamental concepts",
+    courseStrengths = [ "The course is well structured and gave me a great understanding of the fundamental concepts",
                 "It is good for real life applications", "It had enough number of tests to make the course thorough",
                 "It conveys the essence of the subject to the students at its best", "It's content is intuitive",
                 "It is designed to ensure that the students are not left behind", "Overall it was interesting to study the course"
               ];
 
-    x[2].value=scripts[Math.floor(Math.random() * scripts.length)];
+    textBox[2].value=courseStrengths[Math.floor(Math.random() * courseStrengths.length)];
 
-    x[4].value = "No suggestions as such, so far so good";
+    textBox[4].value = "No suggestions as such, so far so good";
 
-    for (i = 5; i < x.length; i++) {  // extra spaces "NIL"ed
-      x[i].value = "NIL";
+    for (i = 5; i < textBox.length; i++) {  // extra spaces "NIL"ed
+      textBox[i].value = "NIL";
     }
 
-    x = myframe.document.querySelectorAll('input[type="radio"]');
+    radioButton = myframe.document.querySelectorAll('input[type="radio"]');
 
-    for (i = 5; i < x.length-10; i+=5){
-      x[i].click();
+    for (i = 5; i < radioButton.length-10; i+=5){
+      radioButton[i].click();
     }
-	x[18].click();
-    x[53].click();
-    x[58].click();
+	radioButton[18].click();
+    radioButton[53].click();
+    radioButton[58].click();
   }catch(err){
     console.log(err);
 }
