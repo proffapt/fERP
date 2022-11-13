aa67f25df2aa925cc1f2e33ce67eaada07ed175f
Arpit Bhardwaj <proffapt@pm.me>
2022-11-13 16:08:12 +0530

    add: Text feedback

--- Negative-Feedback.js
+++ Negative-Feedback.js
@@ -1,4 +1,33 @@
 try{
+    var textBox = myframe.document.querySelectorAll('textarea');
+    var teacherWeaknesses = [ "The teacher is not approachable", "The teacher doesn't make sure the students understand the concepts", 
+					"The teacher ensures that the speed of the lecture is perfect for everyone", "The teacher is less engaging",
+                    "The teacher doesn't release the slides and materials on time", "The teacher gives too much workload in form of assignments"
+                  ];
+
+	// Randomly selecting teacher's weaknesses from the array created above
+    textBox[1].value=teacherWeaknesses[Math.floor(Math.random() * teacherWeaknesses.length)];
+
+    textBox[0].value = "Unfortunately, can not remember any.";  // this is the default Strength
+    textBox[2].value = "Unfortunately, can not remember any.";  // replace with relevant text
+
+    courseWeaknesses = [ "This course is not well structured and assumed the students to already have the basic knowledge of it",
+                "This course doesn't reflect any real life applications for the subject of study",
+                "This course conveys the essence of the subject in a hard to grasp manner", "This course's content is not intuitive",
+                "This course has a steep learning curve", "Overall this course was not interesting to study!"
+              ];
+
+	// Randomly selecting course's weaknesses from the array created above
+    textBox[3].value=courseWeaknesses[Math.floor(Math.random() * courseWeaknesses.length)];
+
+	suggestions = [ "Update the course's content", "Make the course more engaging"];
+
+    textBox[4].value = suggestions[Math.floor(Math.random() * suggestions.length)];
+
+    for (i = 5; i < textBox.length; i++) {  // extra spaces "NIL"ed
+      textBox[i].value = "NIL";
+    }
+
     radioButton = myframe.document.querySelectorAll('input[type="radio"]');
 
     for (i = 1; i < radioButton.length-10; i+=5){
