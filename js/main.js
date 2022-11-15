a5aabb065e75f2955321f56c91c975aad2d88915
proffapt <proffapt@pm.me>
2022-11-15 19:18:53 +0530

    add: working iteration of main.js

--- js/main.js
+++ js/main.js
@@ -1,13 +1,15 @@
 const positiveFeedbackButton = document.getElementById("positive_feedback");
-const neutralFeedbackButton = document.getElementById("netural_feedback");
+const neutralFeedbackButton = document.getElementById("neutral_feedback");
 const negativeFeedbackButton = document.getElementById("negative_feedback");
 
 positiveFeedbackButton.addEventListener("click", () => {
+	browser.tabs.executeScript({ file: "/js/positive_feedback.js" })
+});
 
-	browser.tabs.executeScript({ file: "/js/feedback.js" })
+neutralFeedbackButton.addEventListener("click", () => {
+	browser.tabs.executeScript({ file: "/js/neutral_feedback.js" })
+});
 
-	// if (course == "theory_course") {
-	// 	if (feedback == "positive_feedback"){
-		// }
-	// }
+negativeFeedbackButton.addEventListener("click", () => {
+	browser.tabs.executeScript({ file: "/js/negative_feedback.js" })
 });
