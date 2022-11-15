114e3d5e44d58c2393f847701b90cb254f8147a1
proffapt <proffapt@pm.me>
2022-11-16 01:42:49 +0530

    initial working iteration of one-click(in extension) filler

--- js/neutral_feedback.js
+++ js/neutral_feedback.js
@@ -1,11 +1,64 @@
-var textBox = myframe.document.querySelectorAll('textarea');
-var radioButton = myframe.document.querySelectorAll('input[type="radio"]');
+var all;
+// ***************
+all = true;
+// get all's state from main.js
 
-// Calculating the start index for filling up the radio buttons
-// Basically, ignoring radio buttons designated for selecting profs.
-var start = 0; for (i = 0; radioButton[i].getAttribute('name') == 'check'; i++, start++) { }
+var textBox, radioButton, start;
+
+try {
+	if (!all) {
+		fill_form();
+	} else {
+		console.log("Just fill the captcha, rest I will handle");
+		var course = document.querySelectorAll('a[href="javascript:void(0)"]');
+		var prof = document.querySelectorAll('input[name="check"]');
+		var profCounter = 0;
+		var courseCounter = 0;
+		handleCourse();
+	}
+} catch(err) {
+	console.error(err);
+}
+
+function handleCourse(){
+	course[courseCounter].click(); courseCounter++;
+	handleProf();
+	profCounter = 0;
+	if (courseCounter < course.length){
+		var submitButton = document.getElementById("sub");
+		submitButton.addEventListener("click", handleCourse);
+	} else return; 
+}
+
+function handleProf() {
+	prof[profCounter].click(); profCounter++;
+	fill_form();
+	console.log("Form filled");
+
+	if (profCounter < prof.length){
+		var submitButton = document.getElementById("sub");
+		submitButton.addEventListener("click", handleProf);
+		console.log("Going to next prof");
+	} else return; 
+}
+
+function fill_form(){
+	textBox = document.querySelectorAll('textarea');
+	radioButton = document.querySelectorAll('input[type="radio"]');
+
+	// Calculating the start index for filling up the radio buttons
+	// Basically, ignoring radio buttons designated for selecting profs.
+	start = 0; for (i = 0; radioButton[i].getAttribute('name') == 'check'; i++, start++) { }
+
+	if (textBox.length == 5) theory_course();
+	else lab_course();
+}
 
 function theory_course() {
+	textBox = document.querySelectorAll('textarea');
+	radioButton = document.querySelectorAll('input[type="radio"]');
+	start = 0; for (i = 0; radioButton[i].getAttribute('name') == 'check'; i++, start++) { }
+
 	for (i = 0; i < textBox.length; i++)
 		textBox[i].value = "Nothing as such";
 
@@ -20,6 +73,10 @@ function theory_course() {
 }
 
 function lab_course() {
+	textBox = document.querySelectorAll('textarea');
+	radioButton = document.querySelectorAll('input[type="radio"]');
+	start = 0; for (i = 0; radioButton[i].getAttribute('name') == 'check'; i++, start++) { }
+
 	for (i = 0; i < textBox.length; i++)
 		textBox[i].value = "Nothing as such";
 
@@ -31,11 +88,4 @@ function lab_course() {
 	for (i = start + 47; i < start + 53; i += 5)
 		if (Math.floor(Math.random() * 2)) radioButton[i].click();
 		else radioButton[i + 1].click();
-}
-
-try {
-	if (textBox.length == 5) theory_course();
-	else lab_course();
-} catch(err) {
-	console.error(err);
 }
