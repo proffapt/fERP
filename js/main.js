38e18a526c1be6ec4b458f03803a414c5081f1d5
proffapt <proffapt@pm.me>
2022-11-16 12:29:51 +0530

    final edits for checkbox capabilities

--- js/main.js
+++ js/main.js
@@ -1,28 +1,23 @@
+// getting required elements from the extension
 const positiveFeedbackButton = document.getElementById("positive_feedback_btn");
 const neutralFeedbackButton = document.getElementById("neutral_feedback_btn");
 const negativeFeedbackButton = document.getElementById("negative_feedback_btn");
 const checkBox = document.getElementById("use_for_all");
+// Sending status of checkBox to content scripts
 var isChecked = true;
 checkBox.addEventListener("click", () => {
 	if (checkBox.checked) isChecked = true;
 	else isChecked = false;
 });
-// browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-// 	if (request.allStatus == "getStatusOfAll")
-// 		sendResponse({status: isChecked});
-// });
+browser.runtime.onMessage.addListener((request, sender, sendResponse) => {
+	sendResponse({ status: isChecked });
+});
 
 positiveFeedbackButton.addEventListener("click", () => {
 	browser.tabs.executeScript({ file: "/js/positive_feedback.js" })
 });
 
 neutralFeedbackButton.addEventListener("click", () => {
-	browser.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-	// 	if (request.allStatus == "getStatusOfAll")
-			sendResponse({status: isChecked, feedback: "neutral"});
-		// if (request.feedbackType == "getTypeOfFeedback")
-			// sendResponse({feedback: "neutral"});
-	});
 	browser.tabs.executeScript({ file: "/js/neutral_feedback.js" })
 });
 
