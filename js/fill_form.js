18f83115b1d8f772ef4a929716f97fc5efa10acd
proffapt <proffapt@pm.me>
2022-11-17 10:34:49 +0530

    fix-bug: garbage value of feedback after first iteration

--- js/fill_form.js
+++ js/fill_form.js
@@ -2,12 +2,12 @@ var textBox, radioButton, start, course, prof, profCounter = 0, courseCounter = 
 
 browser.runtime.sendMessage({
 	request: "getStatusOfAll&FeedbackType",
-	}).then((preference) => {
+}).then((preference) => {
 	console.log("User wants to save the preference for feedback type? : ", preference.all);
 	console.log("Feedback Type = ", preference.feedback);
 	try {
 		if (!preference.all) {
-			fill_form();
+			fill_form(preference.feedback);
 			console.log("form filled");
 			console.log("Choose next feedback type");
 		} else {
@@ -15,47 +15,88 @@ browser.runtime.sendMessage({
 			course = document.querySelectorAll('a[href="javascript:void(0)"]');
 			prof = document.querySelectorAll('input[name="check"]');
 			courseCounter = 0;
-			handleCourse();
+			handleCourse(preference.feedback);
 		}
 	} catch (err) {
 		console.error(err);
 	}
 });
 
-function handleCourse() {
+function handleCourse(feedback) {
 	course[courseCounter].click(); courseCounter++;
 	profCounter = 0;
-	handleProf();
+	handleProf(feedback);
+	var submitButton = document.getElementById("sub");
+	var addCourseListener = (x) => {
+		submitButton.addEventListener("click", () => {
+				handleCourse(x);
+			})
+	};
+	var removeCourseListener = (x) => {
+		submitButton.removeEventListener("click", () => {
+				handleCourse(x);
+			})
+	};
+	var removeProfListener = (x) => {
+		submitButton.removeEventListener("click", () => {
+				handleProf(x);
+			})
+	};
+
 	if (courseCounter < course.length) {
-		var submitButton = document.getElementById("sub");
 		if (!profListening) {
-			courseListening = true; submitButton.addEventListener("click", handleCourse);
+			courseListening = true; addCourseListener(feedback);
+			// removeProfListener(feedback);
 			submitButton.removeEventListener("click", handleProf);
 			console.log("Waiting to go to next course, please fill the form");
 		}
 		profListening = false;
 	} else {
+		// removeProfListener(feedback);
 		submitButton.removeEventListener("click", handleProf);
+		// removeCourseListener(feedback);
 		submitButton.removeEventListener("click", handleCourse);
 		console.log("all courses handeled"); return;
 	}
 }
 
-function handleProf() {
+function handleProf(feedback) {
 	prof[profCounter].click(); profCounter++;
-	fill_form();
+	fill_form(feedback);
 	console.log("Form filled");
-
 	var submitButton = document.getElementById("sub");
+	var addCourseListener = (x) => {
+		submitButton.addEventListener("click", () => {
+				handleCourse(x);
+			})
+	};
+	var addProfListener = (x) => {
+		submitButton.addEventListener("click", () => {
+				handleProf(x);
+			})
+	};
+	var removeCourseListener = (x) => {
+		submitButton.removeEventListener("click", () => {
+				handleCourse(x);
+			})
+	};
+	var removeProfListener = (x) => {
+		submitButton.removeEventListener("click", () => {
+				handleProf(x);
+			})
+	};
+
 	if (profCounter < prof.length) {
 		if (!courseListening) {
-			profListening = true; submitButton.addEventListener("click", handleProf);
+			profListening = true; addProfListener(feedback);
+			// removeCourseListener(feedback);
 			submitButton.removeEventListener("click", handleCourse);
 			console.log("Waiting to go to next prof, please fill the form");
 		}
 	} else {
 		if (!profListening) {
-			courseListening = true; submitButton.addEventListener("click", handleCourse);
+			courseListening = true; addCourseListener(feedback);
+			// removeProfListener(feedback);
 			submitButton.removeEventListener("click", handleProf);
 			console.log("all profs in this course handeled");
 			console.log("Waiting to go to next course, please fill the form");
@@ -66,6 +107,7 @@ function handleProf() {
 }
 
 function fill_form(feedback) {
+	console.log("Feedback Type = ", feedback);
 	textBox = document.querySelectorAll('textarea');
 
 	if (textBox.length == 5) {
@@ -94,4 +136,16 @@ function fill_form(feedback) {
 				break;
 		}
 	}
+}
+
+function neutral_theory_feedback() {
+	console.log("doing something");
+}
+
+function positive_theory_feedback() {
+	console.log("doing something");
+}
+
+function negative_theory_feedback() {
+	console.log("doing something");
 }
