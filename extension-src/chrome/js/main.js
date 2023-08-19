2fc8591d0f1ac76fa3802dd552b2d92e76ab2620
harshit-jain52 <harshitjain0502@gmail.com>
2023-08-19 22:35:54 +0530

    update main.js

--- extension-src/chrome/js/main.js
+++ extension-src/chrome/js/main.js
@@ -17,7 +17,7 @@ positiveFeedbackButton.addEventListener("click", () => {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, {action: "getStatusOfAll&FeedbackType", preference: feedback, all: isChecked});
 	});
-	chrome.tabs.executeScript({ file: "/js/feedback.js" })
+	chrome.scripting.executeScript({ files: ["/js/feedback.js"] })
 });
 
 neutralFeedbackButton.addEventListener("click", () => {
@@ -25,7 +25,7 @@ neutralFeedbackButton.addEventListener("click", () => {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, {action: "getStatusOfAll&FeedbackType", preference: feedback, all: isChecked});
 	});
-	chrome.tabs.executeScript({ file: "/js/feedback.js" })
+	chrome.scripting.executeScript({ files: ["/js/feedback.js"] })
 });
 
 negativeFeedbackButton.addEventListener("click", () => {
@@ -33,5 +33,5 @@ negativeFeedbackButton.addEventListener("click", () => {
 	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
 		chrome.tabs.sendMessage(tabs[0].id, {action: "getStatusOfAll&FeedbackType", preference: feedback, all: isChecked});
 	});
-	chrome.tabs.executeScript({ file: "/js/feedback.js" })
+	chrome.scripting.executeScript({ files: ["/js/feedback.js"] })
 });
