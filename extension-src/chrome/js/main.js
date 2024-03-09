0e3ecf4e4a109234f4376c921f01ecf2046fc801
proffapt <proffapt@pm.me>
2024-03-10 03:48:21 +0530

    fix: mulitple dangling listeners on feedback type buttons

--- extension-src/chrome/js/main.js
+++ extension-src/chrome/js/main.js
@@ -13,25 +13,40 @@ checkBox.addEventListener("click", () => {
 var feedback;
 
 positiveFeedbackButton.addEventListener("click", () => {
-	feedback = "positive";
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, {action: "getStatusOfAll&FeedbackType", preference: feedback, all: isChecked});
-		chrome.scripting.executeScript({ target: {tabId: tabs[0].id}, files: ['/js/feedback.js'] });
-	});
+  feedback = "positive";
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.scripting.executeScript({ target: { tabId: tabs[0].id }, files: ['/js/feedback.js'] });
+  });
+  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action === "getStatusOfAll&FeedbackType") {
+      sendResponse({ preference: feedback, all: isChecked });
+    }
+    chrome.runtime.onMessage.removeListener(arguments.callee);
+  });
 });
 
 neutralFeedbackButton.addEventListener("click", () => {
 	feedback = "neutral";
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, {action: "getStatusOfAll&FeedbackType", preference: feedback, all: isChecked});
-		chrome.scripting.executeScript({ target: {tabId: tabs[0].id}, files: ['/js/feedback.js'] });
-	});
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.scripting.executeScript({ target: { tabId: tabs[0].id }, files: ['/js/feedback.js'] });
+  });
+  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action === "getStatusOfAll&FeedbackType") {
+      sendResponse({ preference: feedback, all: isChecked });
+    }
+    chrome.runtime.onMessage.removeListener(arguments.callee);
+  });
 });
 
 negativeFeedbackButton.addEventListener("click", () => {
 	feedback = "negative";
-	chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
-		chrome.tabs.sendMessage(tabs[0].id, {action: "getStatusOfAll&FeedbackType", preference: feedback, all: isChecked});
-		chrome.scripting.executeScript({ target: {tabId: tabs[0].id}, files: ['/js/feedback.js'] });
-	});
+  chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
+    chrome.scripting.executeScript({ target: { tabId: tabs[0].id }, files: ['/js/feedback.js'] });
+  });
+  chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+    if (request.action === "getStatusOfAll&FeedbackType") {
+      sendResponse({ preference: feedback, all: isChecked });
+    }
+    chrome.runtime.onMessage.removeListener(arguments.callee);
+  });
 });
